23db0fca3538be97af87c03c0c0e045128679894
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-09-19 21:24:08 -0300

    Added Welcome Screen layout

--- src/screens/WelcomeScreen/index.js
+++ src/screens/WelcomeScreen/index.js
@@ -1,11 +1,44 @@
+import { Fontisto } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Button from '../../components/Button';
+import Logo from '../../components/Logo';
+import { colors } from '../../styles';
 import styles from './styles';
 
 const WelcomeScreen = () => {
+  async function openBrowser() {
+    await WebBrowser.openBrowserAsync('https://www.meifacil.com/duvidas');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Logo />
+      <View style={[styles.basicView, styles.centeredView]}>
+        <Text style={styles.welcomeTitle}>Bem-vindo!</Text>
+        <Text style={styles.welcomeMessage}>
+          Tudo que você precisa para o seu MEI em um só lugar
+        </Text>
+      </View>
+      <View style={styles.basicView}>
+        <Button
+          text="Tenho CNPJ mei"
+          icon={
+            <Fontisto name="arrow-right-l" size={24} color={colors.Black} />
+          }
+        />
+        <Button
+          text="Abrir um CNPJ mei"
+          icon={
+            <Fontisto name="arrow-right-l" size={24} color={colors.Black} />
+          }
+        />
+        <TouchableOpacity onPress={() => openBrowser()}>
+          <Text style={styles.frequentQuestions}>perguntas frequentes</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- src/screens/WelcomeScreen/styles.js
+++ src/screens/WelcomeScreen/styles.js
@@ -1,11 +1,42 @@
 import { StyleSheet } from 'react-native';
-import colors from '../../styles/colors';
+import { colors, fonts, metrics } from '../../styles/index';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: colors.Primary,
     alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: metrics.bigMargin,
+  },
+  welcomeTitle: {
+    fontFamily: fonts.robotoBold,
+    color: colors.White,
+    fontSize: fonts.big,
+    paddingVertical: metrics.baseMargin,
+  },
+  welcomeMessage: {
+    fontFamily: fonts.robotoRegular,
+    fontSize: fonts.big,
+    color: colors.White,
+    textAlign: 'center',
+    width: '80%',
+    paddingVertical: metrics.doubleBaseMargin,
+  },
+  frequentQuestions: {
+    fontSize: fonts.regular,
+    color: colors.White,
+    textTransform: 'uppercase',
+    letterSpacing: 2,
+    fontFamily: fonts.robotoBold,
+    paddingVertical: metrics.doubleBaseMargin,
+  },
+  basicView: {
+    width: '100%',
+    flex: 1,
+    alignItems: 'center',
+  },
+  centeredView: {
     justifyContent: 'center',
   },
 });
