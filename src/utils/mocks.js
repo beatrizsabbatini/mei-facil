1af436eb71c30cb6b07cdc448b3305583d3f21c4
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-09-21 00:55:55 -0300

    Added mock files

--- src/utils/mocks.js
+++ src/utils/mocks.js
@@ -2,8 +2,10 @@ import Certificate from '../assets/badge-with-a-star.png';
 import BillOfExchange from '../assets/barcode.png';
 import CrownChatBulb from '../assets/bulb.png';
 import CreditCardMachine from '../assets/credit-card-machine.png';
+import CreditCard from '../assets/credit-card.png';
 import DAS from '../assets/das.png';
 import DollarSign from '../assets/dollarSign.png';
+import ExclamationMark from '../assets/exclamation.png';
 import Heart from '../assets/heart.png';
 import Questions from '../assets/information.png';
 import Invoice from '../assets/invoice.png';
@@ -125,5 +127,56 @@ export const blogData = [
       'Qual máquina de cartão é a melhor? Há várias opções no mercado, mas sua escolha deve se adequar ao seu bolso e ao seu tipo de negócio.',
     url:
       'https://blog.meifacil.com/financas/maquina-de-cartao-o-que-considerar/',
+  },
+];
+
+export const notificationsData = [
+  {
+    id: 0,
+    title: 'Os tipos de cliente digitais',
+    description:
+      'Toque aqui, descubra os tipos de clientes da internet e como ganhar dinheiro com cada um deles.',
+    imgPath: ExclamationMark,
+    date: '17 set',
+  },
+  {
+    id: 1,
+    title: 'Live: Conheça o Pix',
+    description:
+      'Nessa sexta, 17h, estaremos AO VIVO no YouTube para tirar suas dúvidas sobre o Pix, uma nova forma de receber.',
+    imgPath: ExclamationMark,
+    date: '11 set',
+  },
+  {
+    id: 2,
+    title: 'Receba dinheiro devolta',
+    description:
+      'Toque e conheça o Pix, um novo jeito de receber dinheiro em segundos, qualquer dia do ano!',
+    imgPath: ExclamationMark,
+    date: '09 set',
+  },
+  {
+    id: 3,
+    title: 'Maquininha com 1% de taxa',
+    description:
+      'Isso mesmo, 1% no débito e crédito nos primeiros 3 meses. E mesmo depois, você continua com as menores taxas do mercado. Confira aqui!',
+    imgPath: CreditCard,
+    date: '11 set',
+  },
+  {
+    id: 4,
+    title: 'Precisa de máquina de cartão?',
+    description:
+      'Isso mesmo, 1% no débito e crédito nos primeiros 3 meses. E mesmo depois, você continua com as menores taxas do mercado. Confira aqui!',
+    imgPath: CreditCard,
+    date: '11 set',
+  },
+  {
+    id: 5,
+    title: 'Live: Conheça o Pix',
+    description:
+      'Nessa sexta, 17h, estaremos AO VIVO no YouTube para tirar suas dúvidas sobre o Pix, uma nova forma de receber.',
+    imgPath: ExclamationMark,
+    date: '11 set',
   },
 ];
