af413baeddc9e7b6e0ce6856dd628632e3db243d
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-09-20 22:02:56 -0300

    Added home screen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
+import {
+  exclusiveContentData,
+  meiData,
+  receiveFromClientsData,
+} from '../../utils/mocks';
+import BlogSection from './components/BlogSection';
+import DASSection from './components/DASSection';
+import HomeCategorySection from './components/HomeCategorySection';
 import PayTaxBox from './components/PayTaxBox';
+import styles from './styles';
 
 const HomeScreen = ({ navigation }) => {
+  const sections = [
+    {
+      id: 0,
+      name: 'Receber de seus clientes',
+      categories: receiveFromClientsData,
+    },
+    { id: 1, name: 'Seu MEI', categories: meiData },
+    { id: 2, name: 'Conteúdo Exclusivo', categories: exclusiveContentData },
+  ];
   return (
-    <View style={{ flex: 1 }}>
+    <ScrollView contentContainerStyle={styles.homeScreenContainer}>
       <PayTaxBox navigation={navigation} />
-    </View>
+      <DASSection />
+      {sections.map((item) => (
+        <HomeCategorySection
+          sectionTitle={item.name}
+          categories={item.categories}
+          key={item.id.toString()}
+        />
+      ))}
+      <BlogSection />
+    </ScrollView>
   );
 };
 
---
+++ src/screens/HomeScreen/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import { metrics } from '../../styles';
+
+const styles = StyleSheet.create({
+  homeScreenContainer: { paddingHorizontal: metrics.bigMargin },
+});
+
+export default styles;
