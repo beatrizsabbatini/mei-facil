acab3dc4c000920e43449ae770097b8532249490
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-09-20 22:02:21 -0300

    Added category section component

---
+++ src/screens/HomeScreen/components/HomeCategorySection/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+import BasicTitle from '../../../../components/BasicTitle';
+import CategoriesList from '../../../../components/CategoriesList';
+import { metrics } from '../../../../styles';
+import styles from './styles';
+
+const HomeCategorySection = ({ categories, sectionTitle }) => {
+  return (
+    <View style={styles.container}>
+      <BasicTitle
+        title={sectionTitle}
+        customStyle={{ marginBottom: metrics.regularMargin }}
+      />
+      <CategoriesList categories={categories} />
+    </View>
+  );
+};
+export default HomeCategorySection;
