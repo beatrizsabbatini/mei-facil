1e2b4d14ddacca73f5c76c3273be4a0768e78dce
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-09-21 00:56:18 -0300

    Fixed layout padding

--- src/screens/HomeScreen/components/BlogSection/index.js
+++ src/screens/HomeScreen/components/BlogSection/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import BasicContainer from '../../../../components/BasicContainer';
 import BasicTitle from '../../../../components/BasicTitle';
+import { metrics } from '../../../../styles';
 import { blogData } from '../../../../utils/mocks';
 import styles from './styles';
 
@@ -13,15 +14,24 @@ const BlogSection = () => {
 
   return (
     <View style={styles.sectionContainer}>
-      <BasicTitle title="Blog da MEI Fácil" />
+      <BasicTitle
+        title="Blog da MEI Fácil"
+        customStyle={{ marginLeft: metrics.doubleBaseMargin }}
+      />
       <FlatList
         data={blogData}
         horizontal
         showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => {
+        renderItem={({ item, index }) => {
           return (
             <TouchableOpacity onPress={() => openBrowser(item.url)}>
-              <BasicContainer key={item.id.toString()} style={styles.container}>
+              <BasicContainer
+                key={item.id.toString()}
+                style={[
+                  styles.container,
+                  index === 0 && { marginLeft: metrics.doubleBaseMargin },
+                ]}
+              >
                 <Text style={styles.title}>{item.title}</Text>
                 <Text style={styles.text}>{item.text}</Text>
               </BasicContainer>
