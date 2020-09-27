0d6f026254d932a50e02272891bbbdc66d7c3722
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-09-26 23:47:53 -0300

    Added duck pattern configuration

---
+++ src/store/ducks/user.js
@@ -0,0 +1,64 @@
+// Action Types
+export const Types = {
+	GET_USER_REQUEST: 'user/get user request',
+	GET_USER_ERRORS: 'user/ get user errors ',
+	GET_USER_SUCCESS: 'user/ get user success',
+};
+
+//Initial state
+const initialState = {
+	data: undefined,
+	errors: undefined,
+	loading: false
+};
+
+//Reducer
+export default function reducer(state = initialState, action) {
+	switch (action.type) {
+	case Types.GET_USER_REQUEST:
+		return {
+			...state,
+			loading: true,
+			errors: undefined,
+			cpf: action.payload
+		};
+	case Types.GET_USER_ERRORS:
+		return {
+			...state,
+			loading: false,
+			errors: action.payload
+		};
+	case Types.GET_USER_SUCCESS:
+		return {
+			...state,
+			errors: undefined,
+			loading: false,
+			data: action.payload
+		};
+
+	default:
+		return state;
+	}
+}
+
+// Action Creators
+export function userRequest(cpf) {
+	return {
+		type: Types.GET_USER_REQUEST,
+		payload: cpf
+	};
+}
+
+export function userErrors(cpf) {
+	return {
+		type: Types.GET_USER_ERRORS,
+		payload: cpf
+	};
+}
+
+export function userSuccess(data) {
+	return {
+		type: Types.GET_USER_SUCCESS,
+		payload: data
+	};
+}
